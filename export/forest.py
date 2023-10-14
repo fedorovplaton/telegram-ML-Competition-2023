8396e3ac79098843d807ad58b6cbf85b06bef4c3
kamaBulletYezhi <kdmazitov@edu.hse.ru>
2023-10-15 03:28:57 +0500

    upd forest exporter

--- export/forest.py
+++ export/forest.py
@@ -9,65 +9,65 @@ FIELD_TO_NAME_C_TYPE: Dict[str, str] = dict(
     children_right=("allRightChildren", "int"),
     threshold=("allThresholds", "double"),
     feature=("allIndices", "int"),
-    value=("allClasses", "int"),
+    value=("allClasses", "unsigned char"),
 )
 
+def compress_value_array(value: np.ndarray):
+    value = np.squeeze(value, axis=(1,)).astype(np.int8)
+    value = value.argmax(-1)
+    return value
 
-def get_values_from_forest(forest: RandomForestClassifier, field: str) -> List[np.ndarray]:
-    res = [
-        dtree.tree_.__getattribute__(field)
+
+def get_values_from_forest(forest: RandomForestClassifier, field: str, dtype: type = int) -> List[np.ndarray]:
+    return [
+        dtree.tree_.__getattribute__(field).astype(dtype)
         for dtree in forest
     ]
-    if field == "value":
-        res = [np.squeeze(arr, axis=(1,)).astype(int) for arr in res]
-    return res
 
 
 def convert_array_to_cpp_const(array: List[np.ndarray], c_type: str, var_name: str) -> str:
-    dim = len(array[0].shape) + 1
-    code_lines = [f"const {'std::vector<' * dim}{c_type}{'>' * dim} {var_name} = {{"]
-    if dim == 2:
-        for arr in array:
-            code_lines.append(
-                f"    {{{', '.join(str(val) for val in arr.tolist())}}},"
-            )
-    else:
-        for arr in array:
-            sub_vectors = [
-                f"{{{', '.join([str(val) for val in row.tolist()])}}}"
-                for row in arr
-            ]
-            code_lines.append("    {" + ", ".join(sub_vectors) + "},")
+    assert len(array[0].shape) == 1
+    max_len = max(len(a) for a in array)
+    code_lines = [f"const std::vector<std::array<{c_type}, {max_len}>> {var_name} = {{"]
+    
+    for arr in array:
+        code_lines.append(
+            f"    {{{', '.join(str(val) for val in arr.tolist())}}},"
+        )
     code_lines.append("};")
     
     return "\n".join(code_lines)
 
 
-def convert_forest_to_cpp(forest: RandomForestClassifier, save_path: Path) -> None:
-    code_blocks = (
-        convert_array_to_cpp_const(
-            get_values_from_forest(forest, field),
-            c_type,
-            var_name
-        )
-        for field, (var_name, c_type) in FIELD_TO_NAME_C_TYPE.items()
-    )
-    with open(save_path, "w") as h_file:
-        h_file.write("".join([
-            "#ifndef FOREST_PARAMS_H\n",
-            "#define FOREST_PARAMS_H\n",
-            "\n"    
-            "#include <vector>\n",
-            "\n"
-        ]))
-        for block in code_blocks:
-            h_file.write(block)
-        h_file.write("\n\n#endif // FOREST_PARAMS_H\n")
+def convert_param_to_cpp(forest: RandomForestClassifier, field: str, dtype: type = int):
+    var_name, c_type = FIELD_TO_NAME_C_TYPE[field]
+    arr_list = get_values_from_forest(forest, field, dtype)
+    if field == "value":
+        arr_list = list(map(compress_value_array, arr_list))
+    code = convert_array_to_cpp_const(arr_list, c_type, var_name)
+    
+    return "\n".join([
+        f"#ifndef {field.upper()}_H",
+        f"#define {field.upper()}_H",
+        "\n", 
+        "#include <vector>",
+        "#include <array>",
+        code,
+        f"\n#endif // {field.upper()}_H\n"
+    ])
+
+
+def make_param_files(forest: RandomForestClassifier, save_dir: Path):
+    save_dir.mkdir(511, 1, 1)
+    for field in FIELD_TO_NAME_C_TYPE:
+        save_path = save_dir / f"{field}.h"
+        dtype = np.float64 if field == "threshold" else int
+        with open(save_path, "w") as file:
+            file.write(convert_param_to_cpp(forest, field, dtype))
+
 
 
 if __name__ == "__main__":
     import joblib
     forest = joblib.load("../forest.pkl")
-    # val = forest[0].tree_.value
-    # print(convert_array_to_cpp_const(val, "int", "allClasses")[:100])
-    convert_forest_to_cpp(forest, "../deploy/forest_params.h")
+    make_param_files(forest, Path("../deploy/"))
