5e558b46efece52d30105213266ce3be91e2df15
kamaBulletYezhi <kdmazitov@edu.hse.ru>
2023-10-15 05:38:54 +0500

    add headers

---
+++ deploy/forest.h
@@ -0,0 +1,11 @@
+#ifndef FOREST_H
+#define FOREST_H
+
+#include <array>
+
+#define N_FEATURES 3000
+#define N_TREES 50
+
+int predict(const std::array<double, N_FEATURES>& features);
+
+#endif // FOREST_H
---
+++ deploy/transform.h
@@ -0,0 +1,12 @@
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+#include <array>
+#include <vector>
+#include <string>
+
+
+std::string preprocess(std::string x);
+std::vector<std::string> tokenize(std::string code_str);
+
+#endif // TRANSFORM_H
